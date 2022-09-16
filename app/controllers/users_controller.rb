b39d4ed5026c49250c1e4ba44611982fdbf2ac79
Andre Bomfim <abomfimx@gmail.com>
2022-09-15 23:53:51 -0300

    Add authentication helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 2)
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to articles_path,
                   notice: "Welcome to A-Blog #{@user.username.capitalize}, you have successfully signed up"
     else
@@ -22,12 +24,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path, notice: 'Your account information was successfully updated'
     else
@@ -39,5 +38,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
